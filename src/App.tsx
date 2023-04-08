1c275ee18b67d73585fa1598c47bf135a136390a
Gabe Jensen <thegabejensen@gmail.com>
2023-04-08 11:25:17 -0700

    Done fetch data

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,21 @@ import { Component, createSignal } from "solid-js";
 
 const App: Component = () => {
   const [first, setFirst] = createSignal("Gabe");
+  const [headerText, setHeaderText] = createSignal("Default Header");
 
   function onInputChange({ target }: KeyboardEvent) {
     const title = (target as HTMLInputElement).value.trim();
     setFirst(title);
+  }
+
+  async function onBtnClick() {
+    const r: Response = await fetch(
+      "https://gabemeister-yousearch-api.onrender.com/"
+    );
+    const data: string = await r.text();
+
+    console.log("\n\n***** data *****\n", data, "\n\n");
+    setHeaderText(data);
   }
 
   return (
@@ -13,6 +24,8 @@ const App: Component = () => {
       <p>This is Gabes first SolidJS project</p>
       <input type="text" onKeyUp={onInputChange} />
       <p>{first()}</p>
+      <button onClick={onBtnClick}>Fetch Data</button>
+      <h1>{headerText()}</h1>
     </div>
   );
 };
